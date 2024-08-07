20e283a6184d620295cf7f0c47505de6cbd036d3
aman0iz0back <dr.amans8932@gmail.com>
2024-08-07 16:49:14 +0530

    Hotstar Clone

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+    name: "falcon and the winter solier",
+    des:
+        "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience",
+    image: "images/slider 2.PNG"
+    },
+    {
+    name: "loki",
+    des:
+        "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patie",
+    image: "images/slider 1.PNG"
+    },
+    {
+    name: "wanda vision",
+    des:
+        "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patie",
+    image: "images/slider 3.PNG"
+    },
+    {
+    name: "raya and the last dragon",
+    des:
+        "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patie",
+    image: "images/slider 4.PNG"
+    },
+    {
+    name: "luca",
+    des:
+        "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patie",
+    image: "images/slider 5.PNG"
+    }
+];
